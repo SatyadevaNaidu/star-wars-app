9ef466ee123ec93f5c2b02f1bf6f95e1a10bbf0b
SatyadevaNaidu <67686345+SatyadevaNaidu@users.noreply.github.com>
2022-03-09 09:03:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import Routing from "./components/Routing";
 import "./styles/style.css";
 
 export default function App() {
+
   const [status, setStatus] = useState(0);
   const [information, setInformation] = useState([]);
   const endpoint = "https://swapi.dev/api/people";
+
   useEffect(() => {
     const getData = async () => {
       try {
+        /* Fetching data from the API */
         const response = await fetch(endpoint);
         const people = await response.json();
-        console.log("oredr", people);
+
+        console.log("check", people);
         setInformation(people.results);
         setStatus(1);
       } catch {
@@ -31,7 +35,7 @@ export default function App() {
       </header>
       {status === 0 ? <h1 style={{ color: 'yellow' }}>Loading...</h1> : null}
       {status === 1 ? <Routing people={information} /> : null}
-      {status === 2 ? <p>Sorry we cannot find data</p> : null}
+      {status === 2 ? <h3 style={{ color: 'yellow' }}>Sorry we cannot find data</h3> : null}
     </div>
   );
 }
