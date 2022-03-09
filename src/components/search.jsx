a3fc00bd24deda426b0a6dff4cdbc4757e23bf3b
SatyadevaNaidu <67686345+SatyadevaNaidu@users.noreply.github.com>
2022-03-09 09:44:18 +0100

    Update search.jsx

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -9,14 +9,17 @@ export default function Search({ people }) {
     return person.name.toLowerCase().includes(searchField.toLowerCase());
   });
 
+  /* Catches string typed in search bar */
   const handleChange = (e) => {
     setSearchField(e.target.value);
   };
 
+  /* Searches for specific character */
   function searchList() {
     return <People people={filteredPersons} />;
   }
 
+  /* Returns search results */
   return (
     <section className="search-section">
       <div className="search-input">
