ade2877a278ed62b70eddf943d0fb5aea6c950cb
SatyadevaNaidu <67686345+SatyadevaNaidu@users.noreply.github.com>
2022-03-09 09:04:01 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ import "../styles/style.css";
 import logo from "../assets/startwars.png";
 
 export default function Header() {
+  /* Header for the page */
   return (
     <header className="main-header">
-      <img className="logo-image" src={logo} alt="TrackIt logo" />
+      <img className="logo-image" src={logo} alt="Starwars logo" />
     </header>
   );
 }
